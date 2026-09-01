b80cf220606324d900a947bd65ac476c6799555e
Zabi Sideqi <zabi.sideqi@gmail.com>
2026-09-01 13:14:07 +0200

    Register endpoint

---
+++ BookQuotes.Api/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using BookQuotes.Api.Data;
+using BookQuotes.Api.DTOs;
+using BookQuotes.Api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookQuotes.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+    private readonly PasswordHasher<User> _passwordHasher;
+
+    public AuthController(ApplicationDbContext context)
+    {
+        _context = context;
+        _passwordHasher = new PasswordHasher<User>();
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterDto dto)
+    {
+        if (await _context.Users.AnyAsync(u => u.Username == dto.Username))
+        {
+            return BadRequest("Username already exists.");
+        }
+
+        var user = new User
+        {
+            Username = dto.Username
+        };
+
+        user.PasswordHash =
+            _passwordHasher.HashPassword(user, dto.Password);
+
+        _context.Users.Add(user);
+
+        await _context.SaveChangesAsync();
+
+        return Ok("User registered successfully.");
+    }
+}
