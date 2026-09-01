25381138c8408f6a2ca68fb39e8f3510218e41e6
Zabi Sideqi <zabi.sideqi@gmail.com>
2026-09-01 12:16:40 +0200

    Configure SQL Server and add Book entity

---
+++ BookQuotes.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BookQuotes.Api.Models;
+namespace BookQuotes.Api.Data
+
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; } 
+    }
+}
